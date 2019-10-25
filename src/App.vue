3392971074ca0204fc4280cb6b842ef4818e7913
wj871287@gmail.com <wj871287@gmail.com>
2019-10-25 10:14:24 +0800

    Created typical components

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,32 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <Typical 
+      v-bind:msg="msg" 
+      v-bind:steps="steps" 
+      v-bind:loop="loop" 
+      v-bind:wrapper="wrapper"
+    />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import Typical from './components/Typical.vue'
 
 export default {
   name: 'app',
+  data: function() {
+    return {
+      msg: "Fucking Awesome !",
+      steps: ['Hello', 1000, 'Hello world!', 500],
+      loop: 1,
+      wrapper: 'span'
+    }
+  },
   components: {
-    HelloWorld
+    Typical
   }
 }
 </script>
---
+++ src/components/Typical.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    {{msg}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Typical',
+  props: {
+    msg: String,
+    steps: Array,
+    wrapper: String,
+    loop: Number
+  }
+}
+</script>
+
+<style>
+
+</style>
